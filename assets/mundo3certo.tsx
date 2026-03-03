89d86fd9ec7097c71ada114d34dba9f85cd6644b
Pedro Henrique de Brito Caraça <pedro.b23@aluno.ifsc.edu.br>
2026-03-02 22:42:57 -0300

    assets

---
+++ assets/mundo3certo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="mundo3certo" tilewidth="32" tileheight="32" tilecount="1036" columns="37">
+ <image source="Gemini_Generated_Image_wfej6nwfej6nwfej.png" width="1184" height="896"/>
+</tileset>
